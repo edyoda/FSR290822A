974a510202994cc05b66c8fbded69bf5d6479ff1
urvashi29 <urvashigolu29@gmail.com>
2022-10-03 21:30:28 +0530

    added lecture-17

---
+++ Lec-17/script.js
@@ -0,0 +1,37 @@
+// comment 
+
+// display js
+// alert('hello');
+// document.write('<h2>hello world</h2>');//testing purpose
+// console.log(10);//debugging code
+
+document.getElementById('display-card').innerHTML = 'Hello';
+
+//variables
+// declaration 
+var a;
+// assigning/initilization of variable
+a = 20;
+
+// declaration and initilization
+//ideal way
+var b = 30;//number
+
+console.log(a + b);//check console
+
+var firstName = 'alina';//string
+
+console.log(firstName);
+
+// declare mutiple variable in single line
+var person = 'alex', age = 20, salary = 20000;
+
+console.log(person, age, salary);
+
+
+var $lastName = 'joe';
+var _designation = 'developer';
+
+document.getElementById('display-card').innerHTML = $lastName;
+
+// var var = '89';//wrong
